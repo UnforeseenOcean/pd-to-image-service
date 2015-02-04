60348285d0b097811e05bc3a821f661d35c4a7ea
Sébastien Piquemal <sebpiq@gmail.com>
2015-02-04 10:03:56 +0000

    better error handling

--- backend/main.js
+++ backend/main.js
@@ -12,11 +12,46 @@ app.set('port', config.port)
 // Setup other routes
 app.route('/render').get(function(req, res) {
   var fetch = req.query.fetch
-  request.get(fetch, function(err, fetchRes) {
-    if (err) throw err
-    res.setHeader('content-type', 'image/svg+xml')
-    res.end(pdfu.renderSvg(pdfu.parse(fetchRes.body)))
-  })
+
+  // Missing param
+  if (!fetch) {
+    res.status(400)
+    res.end('Missing query param "fetch".')
+  } else {
+
+    request.get(fetch, function(err, fetchRes) {
+      // Handle errors
+      if (err) {
+        if (err.code === 'ENOTFOUND') {
+          res.status(400)
+          res.end('Address "' + fetch + '" not found')
+        } else throw err
+      } else if (fetchRes.statusCode === 200) {
+        // Catch parsing errors
+        try {
+          var patch = pdfu.parse(fetchRes.body)
+        } catch(err) {
+          res.status(500)
+          return res.end('Error parsing the patch :\n' + err)
+        }
+        // Catch rendering errors
+        try {
+          var rendered = pdfu.renderSvg(patch)
+        } catch(err) {
+          res.status(500)
+          return res.end('Error rendering the patch :\n' + err)
+        }
+        // Return rendered SVG
+        res.setHeader('content-type', 'image/svg+xml')
+        res.end(rendered)
+
+      // Non-200 responses
+      } else {
+        res.status(400)
+        res.end('Received HTTP ' +  fetchRes.statusCode + ' while trying to fetch ' + fetch + '\n' + fetchRes.body)
+      }
+    })
+  }
 })
 
 app.listen(app.get('port'), function() {
